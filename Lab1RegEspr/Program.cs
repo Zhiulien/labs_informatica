ed479faff38348dcd71f8cca5aa74fb43df35587
Zhiulien Kalosha <jkalosha@gmail.com>
2025-11-23 20:22:55 +0200

    Created RegEspr class and applied console application flow to match regular expressions

--- Lab1RegEspr/Program.cs
+++ Lab1RegEspr/Program.cs
@@ -10,6 +10,31 @@ namespace Lab1RegEspr
     {
         static void Main(string[] args)
         {
+            // Reading regular expression from the console 
+            Console.WriteLine("Ora inserisci l'espressione regolare da utilizzare");
+            string regepr = Console.ReadLine();
+
+            // Creating instance of the class RegEspr and initialize regular expression
+            RegEspr regEspr = new RegEspr(regepr);
+
+            while (true)
+            {
+                Console.WriteLine("Inserisci la stringa su cui verificare il match");
+                string input = Console.ReadLine();
+
+                string res = regEspr.CheckMatch(input);
+                Console.WriteLine("La stringa che hai inserito fa " + res);
+
+                Console.WriteLine("Se vuoi finire, inserisci 'y', e se vuoi continuare, inserisci 'enter'");
+                string continueInput = Console.ReadLine();
+                if ("y".Equals(continueInput))
+                {
+                    Console.WriteLine("Grazie per aver usato la nostra programma");
+                    break;
+                }
+                Console.WriteLine("---------- ---------- ---------- ---------- ---------- ---------- ");
+            }
+            // Console.ReadKey();
         }
     }
 }
---
+++ Lab1RegEspr/RegEspr.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lab1RegEspr
+{
+    class RegEspr
+    {
+        /// <summary>
+        /// l'espressione regolare da utilizzare
+        /// </summary>
+        private string regex;
+
+        /// <summary>
+        /// Getter and Setter for "regex" field
+        /// </summary>
+        public string Regex { get => regex; set => regex = value; }
+
+        public RegEspr()
+        {
+        }
+
+        /// <summary>
+        /// Parametrized constructor to initialize regex
+        /// </summary>
+        /// <param name="regex">l'espressione regolare da utilizzare</param>
+        public RegEspr(string regex)
+        {
+            this.Regex = regex;
+        }
+
+        /**
+         * Metodo che restituisce se una stringa matcha con una espressione regolare
+         * 
+         */
+        /// <summary>
+        /// Metodo che restituisce se una stringa matcha con una espressione regolare 
+        /// </summary>
+        /// <param name="input">la stringa su cui verificare il match</param>
+
+        /// <returns>ritorna la stringa "match" o "mismatch"</returns>
+        public string CheckMatch(string input)
+        {
+            string res;
+            Match results = System.Text.RegularExpressions.Regex.Match(input, regex);
+            if (results.Success)
+            {
+                res = "match";
+            }
+            else
+            {
+                res = "mismatch";
+            }
+            return res;
+        }
+    }
+}
